72cb966547b34dbc616463e943d1b45339bce46e
Datadigicore <datadigicore@gmail.com>
2016-01-13 21:48:31 +0700

    Add SplashScreen and Icon Mobile Apps

---
+++ app/src/main/java/apps/codecamp/biodiversity/SplashScreen.java
@@ -0,0 +1,41 @@
+package apps.codecamp.biodiversity;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    public static final int detik = 5;
+    public static final int milidetik = detik*1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash);
+        progressAnimasi();
+    }
+    public void progressAnimasi()
+    {
+        new CountDownTimer(milidetik,1000)
+        {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                process_progress(millisUntilFinished);
+            }
+            @Override
+            public void onFinish() {
+                Intent newform = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(newform);
+                finish();
+            }
+        }.start();
+    }
+
+    public int process_progress(long miliseconds)
+    {
+        return (int) ((milidetik-miliseconds)/1000);
+    }
+}
